fixed issues with double scaling during prediction. Ran tests with actual default configurations to reproduce results

Dataset.py
@@ -172,5 +172,5 @@ class Dataset:
     ):
         x_tr, y_tr, x_te, y_te = self.split(test_ids, by)
         x_tr = self.scale(x_tr, fit=True)
-        x_te = self.scale(x_te, fit=False)
+        # x_te = self.scale(x_te, fit=False)
         return x_tr.values, x_te.values, np.array(y_tr), np.array(y_te), self.scaler

Model.py
@@ -138,13 +138,10 @@ class EnsembleModel:
         else:
             self.weights = weights
 
-    def subsample_data(self, X_data, y_data, proportion):
-        # Subsample the data based on the given proportion
-        if not (0 < proportion < 1):
-            raise ValueError("Proportion must be between 0 and 1.")
-        num_samples = int(len(X_data) * proportion)
-        return X_data[:num_samples], y_data[:num_samples]
-    
+    def subsample_data(self, X, y, proportion):
+        rows = np.random.choice(X.shape[0], size = int(X.shape[0]*proportion)) # Randomly choose 70% of data
+        return X[rows,:], y[rows]
+
     def bootstrap_data(self, X_data, y_data, proportion):
         if not (0 < proportion < 1):
             raise ValueError("Proportion must be between 0 and 1.")